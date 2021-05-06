ab59ee7a2ee88d5b40489beb5f211a6063ea6a6d
Pedro <pedro.rhamon@dataprev.gov.br>
2021-05-06 16:22:08 -0300

    Projeto em angular, melhorias em breve

---
+++ src/app/components/project/project-read2/project-read2.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ProjectRead2DataSource, ProjectRead2Item } from './project-read2-datasource';
+
+@Component({
+  selector: 'app-project-read2',
+  templateUrl: './project-read2.component.html',
+  styleUrls: ['./project-read2.component.css']
+})
+export class ProjectRead2Component implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ProjectRead2Item>;
+  dataSource: ProjectRead2DataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id','nome', 'situacao', 'viabilidade','dataInicio','dataFinal', "action"];
+
+  ngOninit() {
+    this.dataSource = new ProjectRead2DataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
---
+++ src/app/components/project/project.model.ts
@@ -0,0 +1,8 @@
+export interface Project {
+    id?: number
+    nome: string
+    situacao: string
+    viabilidade: number
+    dataInicio: string
+    dataFinal: string
+}
